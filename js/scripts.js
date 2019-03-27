5d8c86c0d8ae611964db6ea98f805fa5ff34bbf7
Kerriann Walker and Marc Davies <kerriann@>
2019-03-27 16:39:14 -0700

    create appointment booking site

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-$(document).ready() {
+//var name= $("#name").val();
+//var date= $("#date").val();
+//var time= $("#time").val();
 
-};
+$(document).ready(function() {
+	$("form#appointment").submit(function(event){
+		var name = $("input#name").val();
+    console.log(name);
+		var description= $("input#description").val();
+    console.log(description);
+    var date = $("input#date").val();
+    console.log(date);
+		var time= $("input#time").val();
+    console.log(time);
+    $("#output").text("Your booking is confirmed!");
+    var message = "Hi " + name + " your appointment is confirmed for " + date;
+    $("#output").text(message);
+    event.preventDefault();
+  });
+});
