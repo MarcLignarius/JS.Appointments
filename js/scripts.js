cbc3dce140ddb768c294746758b5a851936b29f6
Marc Davies <marcdaviesriot@gmail.com>
2019-03-28 01:27:51 -0700

    Minor code refactor

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,10 @@
-//var name= $("#name").val();
-//var date= $("#date").val();
-//var time= $("#time").val();
-
 $(document).ready(function() {
 	$("form#appointment").submit(function(event){
 		var name = $("input#name").val();
-    console.log(name);
-		var description= $("input#description").val();
-    console.log(description);
+		var email= $("input#email").val();
     var date = $("input#date").val();
-    console.log(date);
 		var time= $("input#time").val();
-    console.log(time);
-    $("#output").text("Your booking is confirmed!");
-    var message = "Hi " + name + " your appointment is confirmed for " + date;
-    $("#output").text(message);
+    $("#output").text("Thank you " + name + ". " + "Your appointment is confirmed for " + date + " at " + time + ". An email will be sent to " + email + ".")
     event.preventDefault();
   });
 });
